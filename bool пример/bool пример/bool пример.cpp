3e38e3050cc153aef18d818548f56f7ca08d463d
Andrei <dasd_78@mail.ru>
2023-03-20 20:43:53 +0300

    bool примеры

---
+++ bool пример/bool пример/bool пример.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+
+int main()
+{
+	std::cout << true << std::endl;//вместо true еденица
+	std::cout << !true << std::endl;//вместо !true ноль
+
+	bool b(false);
+	std::cout << b << std::endl;// b - false (0)
+	std::cout << !b << std::endl;//!b - true(1)
+
+	std::cout << true << std::endl;
+	std::cout << false << std::endl;
+
+	std::cout << std::boolalpha;//выводим логические значения как "true" 
+	// или "false"
+
+	std::cout << true << std::endl;
+	std::cout << false << std::endl;
+
+	if (true)//это условие
+		std::cout << "The conditeon is true !" << std::endl;
+	else
+		std::cout << "The conditeon is false !" << std::endl;
+
+	
+	if (b)
+		std::cout << "b is true !" << std::endl;
+	else
+		std::cout << "b is false !" << std::endl;
+
+	return 0;
+
+
+}
